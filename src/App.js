2da4d5c0cc822bacb23160ad245761f5f01840c0
Aman Kumar <hijackhacker24by7@gmail.com>
2024-10-27 17:30:02 +0530

    Bug fixed

--- src/App.js
+++ src/App.js
@@ -7,17 +7,19 @@ import {
   BrowserRouter as Router,
   Routes as Switch,
   Route,
-  Link
 } from "react-router-dom";
+
+
 function App() {
   return (
     <Router>
       <Switch>
-        <Route exact path="/" element={ <Login />}></Route>
-        <Route exact path="/Signup" element={ <Signup />} />
+        <Route exact path="/" element={<Login />}></Route>
+        <Route exact path="/Signup" element={<Signup />} />
       </Switch>
     </Router>
-=======
+  );
+}
 
 
 
