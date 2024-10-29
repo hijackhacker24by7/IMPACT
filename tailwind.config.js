bd1707fd2bb2a8fea1b5327843093a36f7814312
Aman Kumar <hijackhacker24by7@gmail.com>
2024-10-29 16:43:45 +0530

    Signup screen

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,13 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
+ 
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'gradient-135': 'linear-gradient(135deg, #7a4d98, #333333)',
+      },
+    },
   },
   plugins: [
     require('@tailwindcss/forms')
