ec5f8e2cb7ec57afaad177a3481d405ce0bd208a
Aman Kumar <hijackhacker24by7@gmail.com>
2024-11-17 18:18:46 +0530

    bug fixed

--- src/screens/Global_chat_box.js
+++ src/screens/Global_chat_box.js
@@ -1,27 +1,15 @@
 
-
-import { BrowserRouter as Router, Route,   Routes as Switch, useHistory } from 'react-router-dom';
-import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import QueryList from './QueryList';
-import QueryDetail from './QueryDetail';
+
 
 function Global_chat_box() {
   return (
-  
-  <div>
-    <QueryList></QueryList>
-  
 
-  
-  </div>
-   
+    <div>
+      <QueryList></QueryList>
+    </div>
+
   );
 }
 
 export default Global_chat_box;
-
-// CSS
-// .fade-enter { opacity: 0; }
-// .fade-enter-active { opacity: 1; transition: opacity 300ms; }
-// .fade-exit { opacity: 1; }
-// .fade-exit-active { opacity: 0; transition: opacity 300ms; }
